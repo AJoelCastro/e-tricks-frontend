8ae0bc70a83402de61e7aaa5c8b6df16e8354ce7
AJoelCastro <misthorn33@gmail.com>
2025-05-30 21:37:42 -0500

    react bootstrap

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MainCarouselComponent from './carousel/MainCarousel'
 
 const MainComponent = () => {
   return (
     <div>
-      
+      <MainCarouselComponent/>
     </div>
   )
 }
