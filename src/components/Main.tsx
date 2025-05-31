3cc66b30e30d068fcdf6e2053a149cedaf45e756
AJoelCastro <misthorn33@gmail.com>
2025-05-30 22:45:55 -0500

    main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,66 @@
 import React from 'react'
 import MainCarouselComponent from './carousel/MainCarousel'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const MainComponent = () => {
   return (
-    <div>
+    <div className='bg-white'>
       <MainCarouselComponent/>
+      <div className='mt-4 grid grid-cols-3'>
+        <div className='col-span-1 relative'>
+          <Image
+            src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80'
+            alt="Next.js logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+
+          <Link 
+            href='/' 
+            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition'
+          >
+            Descubre nuestros productos
+          </Link>
+        </div>
+
+        <div className='col-span-1 relative'>
+          <Image
+            src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80'
+            alt="Next.js logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+
+          <Link 
+            href='/' 
+            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition'
+          >
+            Descubre nuestros productos
+          </Link>
+        </div>
+        <div className='col-span-1 relative'>
+          <Image
+            src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80'
+            alt="Next.js logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+
+          <Link 
+            href='/' 
+            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition'
+          >
+            Descubre nuestros productos
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
