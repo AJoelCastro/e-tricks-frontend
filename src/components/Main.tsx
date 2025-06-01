beb2c225f653c89149f6de25637f126cd28e73fc
AJoelCastro <misthorn33@gmail.com>
2025-06-01 14:00:26 -0500

    cambios button issue #2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -17,13 +17,16 @@ const MainComponent = () => {
             sizes="100vw"
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
-
-          <Link 
-            href='/' 
-            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition'
+          <div 
+            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
           >
-            Descubre nuestros productos
-          </Link>
+            <Link 
+              href='/' 
+              className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
+            >
+              HASTA 40% OFF
+            </Link>
+          </div>
         </div>
 
         <div className='col-span-1 relative'>
@@ -41,9 +44,9 @@ const MainComponent = () => {
           >
             <Link 
               href='/' 
-              className='text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition bg-red-200 '
+              className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
             >
-              Descubre nuestros productos
+              HASTA 40% OFF
             </Link>
           </div>
         </div>
@@ -56,13 +59,16 @@ const MainComponent = () => {
             sizes="100vw"
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
-
-          <Link 
-            href='/' 
-            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition'
+          <div 
+            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
           >
-            Descubre nuestros productos
-          </Link>
+            <Link 
+              href='/' 
+              className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
+            >
+              HASTA 40% OFF
+            </Link>
+          </div>
         </div>
       </div>
     </div>
