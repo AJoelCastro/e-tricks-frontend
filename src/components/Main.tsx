aaea49e49bb28af8c474810250879773ee0b4716
AJoelCastro <misthorn33@gmail.com>
2025-06-01 14:07:55 -0500

    data images

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,74 +2,37 @@ import React from 'react'
 import MainCarouselComponent from './carousel/MainCarousel'
 import Image from 'next/image'
 import Link from 'next/link'
+import { imagesPrueba } from '@/data/ImagesPrueba'
 
 const MainComponent = () => {
   return (
     <div className='bg-white'>
       <MainCarouselComponent/>
       <div className='mt-4 grid grid-cols-3'>
-        <div className='col-span-1 relative'>
-          <Image
-            src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80'
-            alt="Next.js logo"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          />
-          <div 
-            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
-          >
-            <Link 
-              href='/' 
-              className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
-            >
-              HASTA 40% OFF
-            </Link>
-          </div>
-        </div>
-
-        <div className='col-span-1 relative'>
-          <Image
-            src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80'
-            alt="Next.js logo"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          />
-
-          <div 
-            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
-          >
-            <Link 
-              href='/' 
-              className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
-            >
-              HASTA 40% OFF
-            </Link>
-          </div>
-        </div>
-        <div className='col-span-1 relative'>
-          <Image
-            src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80'
-            alt="Next.js logo"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          />
-          <div 
-            className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
-          >
-            <Link 
-              href='/' 
-              className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
-            >
-              HASTA 40% OFF
-            </Link>
-          </div>
-        </div>
+        {
+          imagesPrueba.map((image, index) => (
+            <div className='col-span-1 relative' key={index}>
+              <Image
+                src={image.url}
+                alt="Next.js logo"
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+              <div 
+                className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
+              >
+                <Link 
+                  href='/' 
+                  className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
+                >
+                  HASTA 40% OFF
+                </Link>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
---
+++ src/data/ImagesPrueba.tsx
@@ -0,0 +1,14 @@
+export const imagesPrueba = [
+    {
+        key: 1,
+        url: 'https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80',
+    },
+    {
+        key: 2,
+        url: 'https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80',
+    },
+    {
+        key: 3,
+        url: 'https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80',  
+    }
+  ]
