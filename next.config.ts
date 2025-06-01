4b9aa9b577daaba79b24462b09f7ac4a19e70759
AJoelCastro <misthorn33@gmail.com>
2025-06-01 14:55:58 -0500

    next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["www.bata.com", "source.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.bata.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      }
+    ],
   },
 };
 
