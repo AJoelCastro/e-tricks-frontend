b8c1032f982b25cdf83dacf6afba530b69ab511d
AJoelCastro <misthorn33@gmail.com>
2025-06-01 14:52:18 -0500

    arrelogs link

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,26 +11,30 @@ const MainComponent = () => {
       <div className='mt-4 grid grid-cols-3'>
         {
           imagesPrueba.map((image, index) => (
-            <div className='col-span-1 relative' key={index}>
-              <Image
-                src={image.url}
-                alt="Next.js logo"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-              />
-              <div 
-                className='absolute inset-0 flex items-center justify-center text-white text-xl font-bold bg-opacity-40 hover:bg-opacity-60 transition '
-              >
-                <Link 
-                  href='/' 
-                  className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
-                >
-                  HASTA 40% OFF
+              <div className='col-span-1 relative' key={index}>
+                <Link href='/' >
+                  <Image
+                    src={image.url}
+                    alt="Next.js logo"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                  />
+                  <div 
+                    className='absolute inset-0 flex items-center justify-center bg-black/20 hover:bg-black/35 transition flex-col gap-3'
+                  >
+                    <p 
+                      className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
+                    >
+                      HASTA 40% OFF
+                    </p>
+                    <p className='font-bold text-5xl'>
+                      {image.title}
+                    </p>
+                  </div>
                 </Link>
               </div>
-            </div>
           ))
         }
       </div>
--- src/data/ImagesPrueba.tsx
+++ src/data/ImagesPrueba.tsx
@@ -2,13 +2,16 @@ export const imagesPrueba = [
     {
         key: 1,
         url: 'https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80',
+        title: 'Casual',
     },
     {
         key: 2,
         url: 'https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80',
+        title: 'Deportivo',
     },
     {
         key: 3,
         url: 'https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-pe-Library/es_PE/dw383b8e58/homepage/1.jpg?sw=2560&q=80',  
+        title: 'Formal',
     }
   ]
