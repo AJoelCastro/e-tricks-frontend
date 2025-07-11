9de3cf07f17f6613d54756c67b570db0ec6e80ea
AJoelCastro <misthorn33@gmail.com>
2025-07-11 17:26:08 -0500

    useUser

--- src/components/favorites/LeftSide.tsx
+++ src/components/favorites/LeftSide.tsx
@@ -1,4 +1,5 @@
 import theme from '@/theme/create-theme';
+import { useUser } from '@clerk/nextjs';
 import { Divider, List, ListItem, styled, Typography  } from '@mui/material'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -26,65 +27,67 @@ const StyledNavItem = styled(Link, {
   })
 }));
 const LeftSide = () => {
-const pathname = usePathname();
+
+    const { user } = useUser();
+    const pathname = usePathname();
     const isActive = (path: string) => pathname === path;
-  return (
-    <div className='h-[100vh]  w-[25vw] flex flex-col gap-[4vh]'>
-        <div className='flex mx-8 justify-center items-center rounded-lg'>
-            <Typography variant='h2' >
-                MIS FAVORITOS
-            </Typography>
+    return (
+        <div className='h-[100vh]  w-[25vw] flex flex-col gap-[4vh]'>
+            <div className='flex mx-8 justify-center items-center rounded-lg'>
+                <Typography variant='h2' >
+                    MIS FAVORITOS
+                </Typography>
+            </div>
+            <div className='flex mx-8 justify-center items-center bg-white rounded-xl px-8 py-6'>
+                <Typography variant='hEspecial'>
+                    HOLA, {user?.firstName?.toUpperCase()}
+                </Typography>
+            </div>
+            <div className='px-8'>
+                <List sx={style} aria-label="mailbox folders">
+                    <ListItem>
+                        <StyledNavItem href="/favorites" isActive={isActive('/favorites')}>
+                            <Typography>
+                                MIS FAVORITOS
+                            </Typography>
+                        </StyledNavItem>
+                    </ListItem>
+                    <Divider component="li" />
+                    <ListItem>
+                        <StyledNavItem href="/drafts" isActive={isActive('/drafts')}>
+                            <Typography>
+                                MIS COMPRAS
+                            </Typography>
+                        </StyledNavItem>
+                    </ListItem>
+                    <Divider component="li" />
+                    <ListItem>
+                        <StyledNavItem href="/trash" isActive={isActive('/trash')}>
+                            <Typography>
+                                DIRECCIONES
+                            </Typography>
+                        </StyledNavItem>
+                    </ListItem>
+                    <Divider component="li" />
+                    <ListItem>
+                        <StyledNavItem href="/spam" isActive={isActive('/spam')}>
+                            <Typography>
+                                METODOS DE PAGO
+                            </Typography>
+                        </StyledNavItem>
+                    </ListItem>
+                    <Divider component="li" />
+                    <ListItem>
+                        <StyledNavItem href="/spam" isActive={isActive('/spam')}>
+                            <Typography>
+                                MIS PUNTOS
+                            </Typography>
+                        </StyledNavItem>
+                    </ListItem>
+                </List>
+            </div>
         </div>
-        <div className='flex mx-8 justify-center items-center bg-white rounded-xl px-8 py-6'>
-            <Typography variant='hEspecial'>
-                HOLA, GREICY CHACON
-            </Typography>
-        </div>
-        <div className='px-8'>
-            <List sx={style} aria-label="mailbox folders">
-                <ListItem>
-                    <StyledNavItem href="/favorites" isActive={isActive('/favorites')}>
-                        <Typography>
-                            MIS FAVORITOS
-                        </Typography>
-                    </StyledNavItem>
-                </ListItem>
-                <Divider component="li" />
-                <ListItem>
-                    <StyledNavItem href="/drafts" isActive={isActive('/drafts')}>
-                        <Typography>
-                            MIS COMPRAS
-                        </Typography>
-                    </StyledNavItem>
-                </ListItem>
-                <Divider component="li" />
-                <ListItem>
-                    <StyledNavItem href="/trash" isActive={isActive('/trash')}>
-                        <Typography>
-                            DIRECCIONES
-                        </Typography>
-                    </StyledNavItem>
-                </ListItem>
-                <Divider component="li" />
-                <ListItem>
-                    <StyledNavItem href="/spam" isActive={isActive('/spam')}>
-                        <Typography>
-                            METODOS DE PAGO
-                        </Typography>
-                    </StyledNavItem>
-                </ListItem>
-                <Divider component="li" />
-                <ListItem>
-                    <StyledNavItem href="/spam" isActive={isActive('/spam')}>
-                        <Typography>
-                            MIS PUNTOS
-                        </Typography>
-                    </StyledNavItem>
-                </ListItem>
-            </List>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default LeftSide
--- src/components/favorites/RightSide.tsx
+++ src/components/favorites/RightSide.tsx
@@ -1,26 +1,27 @@
-import { Button, ListItemButton, Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import React from 'react'
 
 const RightSide = () => {
-  return (
-    <div className='w-[75vw]'>
-        <div className='flex mx-8 justify-between items-center bg-white rounded-xl px-8 py-4'>
-            <Typography variant='h7'>
-                FILTRAR POR
-            </Typography>
-            <Button >
+
+    return (
+        <div className='w-[75vw]'>
+            <div className='flex mx-8 justify-between items-center bg-white rounded-xl px-8 py-4'>
                 <Typography variant='h7'>
-                    NOMBRE
+                    FILTRAR POR
                 </Typography>
-            </Button>
-            <Button>
-                <Typography variant='h7'>
-                   MARCA
-                </Typography>
-            </Button>
+                <Button >
+                    <Typography variant='h7'>
+                        NOMBRE
+                    </Typography>
+                </Button>
+                <Button>
+                    <Typography variant='h7'>
+                    MARCA
+                    </Typography>
+                </Button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default RightSide
