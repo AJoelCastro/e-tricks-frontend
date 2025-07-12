181eb9e1b92b93a372214125b7181d3eeb546695
AJoelCastro <misthorn33@gmail.com>
2025-07-12 09:53:03 -0500

    rightside

--- src/components/favorites/RightSide.tsx
+++ src/components/favorites/RightSide.tsx
@@ -1,14 +1,27 @@
-import { Button, Typography } from '@mui/material'
+import { Box, Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 const RightSide = () => {
 
     return (
-        <div >
-            <div className='flex mx-8 justify-between items-center bg-white rounded-xl px-8 py-4'>
+        <Grid container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', borderRadius: 2, marginX: 4, marginBottom: 4, marginTop:2, paddingY: 1 }}>
+
+            <Grid size={{
+                sm:12,
+                md: 3
+            }}
+            sx={{paddingX: 4}}
+            >
                 <Typography variant='h7'>
                     FILTRAR POR
                 </Typography>
+            </Grid>
+            <Grid size={{
+                sm:12,
+                md: 9
+            }}
+            sx={{ display:'flex', flexDirection:'row', justifyContent: 'space-between', paddingX:4 }}
+            >
                 <Button >
                     <Typography variant='h7'>
                         NOMBRE
@@ -16,11 +29,26 @@ const RightSide = () => {
                 </Button>
                 <Button>
                     <Typography variant='h7'>
-                    MARCA
+                        PRECIO
                     </Typography>
                 </Button>
-            </div>
-        </div>
+                <Button>
+                    <Typography variant='h7'>
+                        MARCA
+                    </Typography>
+                </Button>
+                <Button>
+                    <Typography variant='h7'>
+                        CATEGORIA
+                    </Typography>
+                </Button>
+                <Button>
+                    <Typography variant='h7'>
+                        TEMPORADA
+                    </Typography>
+                </Button>
+            </Grid>
+        </Grid>
     )
 }
 
