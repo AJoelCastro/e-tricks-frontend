8cf07816c192f85350e6bb32c1d31ea077be0277
AJoelCastro <misthorn33@gmail.com>
2025-07-14 00:35:25 -0500

    letra arreglos

--- src/components/sections/ThreeImages.tsx
+++ src/components/sections/ThreeImages.tsx
@@ -1,47 +1,49 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 type Image = {
   title: string;
   url: string;
   to: string;
-}
+};
+
 type Props = {
   image: Image;
-}
+};
 
-const ThreeImages: React.FC<Props> = ({image}) => {
+const ThreeImages: React.FC<Props> = ({ image }) => {
   return (
-    <Box sx={{position:'relative'}}>
-        <Link href={image.to} >
-            <Image
-                src={image.url}
-                alt="Next.js logo"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-            />
-            <div 
-                className='absolute inset-0 flex items-center justify-center bg-black/20 hover:bg-black/35 transition flex-col gap-3'
-            >
-              <p 
-                  className='text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm'
-              >
-                  HASTA 40% OFF
-              </p>
-              <p className='font-bold text-5xl'>
-                  {image.title}
-              </p>
-              <p className='font-ligth text-md underline '>
-                  Ver más
-              </p>
-            </div>
-        </Link>
+    <Box sx={{ position: 'relative' }}>
+      <Link href={image.to}>
+        <Image
+          src={image.url}
+          alt="Next.js logo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+        <Box
+          className="absolute inset-0 flex items-center justify-center bg-black/20 hover:bg-black/35 transition flex-col gap-3"
+        >
+          <Typography
+            className="text-white text-sm font-semibold bg-opacity-40 hover:bg-opacity-60 transition bg-red-600 px-3 py-1 rounded-sm"
+            sx={{ pointerEvents: 'none', color: 'white', fontFamily:'monospace'  }}
+          >
+            HASTA -40% OFF
+          </Typography>
+          <Typography
+            variant='h2'
+            sx={{ pointerEvents: 'none', color: 'white', fontFamily:'monospace' }}
+          >
+            {image.title}
+          </Typography>
+        </Box>
+      </Link>
     </Box>
-  )
-}
+  );
+};
 
-export default ThreeImages
+export default ThreeImages;
