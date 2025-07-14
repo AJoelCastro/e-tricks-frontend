8caf6dcc7759e98b4276636cc2fbdaaa26d1ce16
AJoelCastro <misthorn33@gmail.com>
2025-07-14 13:55:51 -0500

    cart section issue #8

--- src/components/cart/RightSide.tsx
+++ src/components/cart/RightSide.tsx
@@ -2,6 +2,7 @@ import UserService from '@/services/UserService'
 import { Box, Button, CircularProgress, Grid, Typography } from '@mui/material'
 import React, { useCallback, useEffect, useState } from 'react'
 import ProductCard from '../cards/Products';
+import CartProgress from './Stepper';
 
 const RightSideCart = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -22,48 +23,29 @@ const RightSideCart = () => {
     
     return (
         <Box>
-            <Grid container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', borderRadius: 2, marginX: 4, marginBottom: 4, marginTop:2, paddingY: 1 }}>
+            <Grid container sx={{ marginX: 2, marginBottom: 4, marginTop:2, paddingY: 1 }} spacing={2}>
                 <Grid size={{
-                    sm:12,
-                    md: 3
+                    xs:12,
+                    sm:6,
+                    md:7
                 }}
-                sx={{paddingX: 4}}
+                sx={{paddingX: 2, backgroundColor:'white',borderRadius: 2, paddingTop:2}}
                 >
-                    <Typography variant='h7'>
-                        FILTRAR POR
-                    </Typography>
+                    <CartProgress activeStep={0}/>
                 </Grid>
                 <Grid size={{
-                    sm:12,
-                    md: 9
+                    xs:12,
+                    sm:6,
+                    md:5
                 }}
-                sx={{ display:'flex', flexDirection:'row', justifyContent: 'space-between', paddingX:4 }}
+                sx={{ paddingX:2, backgroundColor:'white',borderRadius: 2, paddingTop:2 }}
                 >
-                    <Button >
-                        <Typography variant='h7'>
-                            NOMBRE
+                    <Box sx={{display:'flex', justifyContent:'center'}}>
+                        <Typography variant='h6'>
+                            DETALLES DE LA COMPRA
                         </Typography>
-                    </Button>
-                    <Button>
-                        <Typography variant='h7'>
-                            PRECIO
-                        </Typography>
-                    </Button>
-                    <Button>
-                        <Typography variant='h7'>
-                            MARCA
-                        </Typography>
-                    </Button>
-                    <Button>
-                        <Typography variant='h7'>
-                            CATEGORIA
-                        </Typography>
-                    </Button>
-                    <Button>
-                        <Typography variant='h7'>
-                            TEMPORADA
-                        </Typography>
-                    </Button>
+                    </Box>
+                    
                 </Grid>
             </Grid>
             <Grid container sx={{ marginX: 4, marginBottom: 4 }} spacing={3}>
