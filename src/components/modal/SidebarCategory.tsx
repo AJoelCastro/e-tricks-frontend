5fe7701e4ebc01e0fbad4b0f23831e67b875bd6d
AJoelCastro <misthorn33@gmail.com>
2025-07-25 21:49:02 -0500

    sidebar route

--- src/components/modal/SidebarCategory.tsx
+++ src/components/modal/SidebarCategory.tsx
@@ -187,25 +187,6 @@ const SidebarCategory = ({
                     </Link>
                   </li>
                 </ul>
-                <hr className="my-3 border-gray-200 w-full" />
-                <ul className="space-y-2">
-                  <li className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
-                    <Link href="/stores" className="text-gray-800 hover:text-[#7950f2]">
-                      Mi cuenta
-                    </Link>
-                  </li>
-                  <li className="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    <Link href="/favoritos" className="text-gray-800 hover:text-[#7950f2]">
-                      Favoritos
-                    </Link>
-                  </li>
-                </ul>
               </div>
             </div>
 
@@ -222,11 +203,11 @@ const SidebarCategory = ({
                       ?.categories.map((cat) => (
                         <li key={`cat-${cat._id}`}>
                           <Link
-                            href={`/products?brand=${selectedBrandId}&category=${cat._id}`}
+                            href={`/mujer/${selectedSubCategory?.routeLink}/${cat.name.toLowerCase()}`}
                             className="block w-full p-2 hover:text-[#7950f2] transition-colors"
                             onClick={onClose}
                           >
-                            <Typography variant="reseniasCard">{cat.name}</Typography>
+                            <Typography variant="priceCard">{cat.name}</Typography>
                           </Link>
                         </li>
                       )) || (
