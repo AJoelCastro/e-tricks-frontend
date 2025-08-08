399a44f1770bf223d900f2f02a0a4b5f17b4afd9
AJoelCastro <misthorn33@gmail.com>
2025-08-07 22:40:24 -0500

    client

--- src/page-sections/cart/CartContext.tsx
+++ src/page-sections/cart/CartContext.tsx
@@ -1,3 +1,4 @@
+'use client'
 // components/cart/CartContext.tsx
 import React, { createContext, useContext, useState, useEffect, useCallback } from 'react';
 import { usePathname } from 'next/navigation';
